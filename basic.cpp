a0d6d4b244b13e7b082176dd1c0c398ba6a2b1c9
Schiffers <cesar.a.c.c@hotmail.com>
2017-05-23 13:17:06 -0300

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,21 @@
+void ProtocolGameBase::sendBasicData()
+{
+	NetworkMessage msg;
+	msg.addByte(0x9F);
+	if (player->isPremium()) {
+		msg.addByte(1);
+		msg.add<uint32_t>(time(nullptr) + (player->premiumDays * 86400));
+	} else {
+		msg.addByte(0);
+		msg.add<uint32_t>(0);
+	}
+	msg.addByte(player->getVocation()->getClientId());
+	if (version > 1099) {
+		msg.addByte(1); // has reached Main (allow player to open Prey window)
+	}
+	msg.add<uint16_t>(0xFF); // number of known spells
+	for (uint8_t spellId = 0x00; spellId < 0xFF; spellId++) {
+		msg.addByte(spellId);
+	}
+	writeToOutputBuffer(msg);
+}
